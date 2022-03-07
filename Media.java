71529f3f83362024bb35432e53961dd94830e45e
Matheus Muccio <matheus_muccio@outlook.com>
2022-03-07 17:11:55 +0000

    Salvando

--- Media.java
+++ Media.java
@@ -1,7 +1,24 @@
+import java.util.Scanner;
+
+
 public class Media {
     public static void main(String[] args){
-        double media = 7.55;
-        System.out.printf("Média = %.2f", media);
+
+        double n1;
+        double n2;
+        double media;
+
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Digite o primeiro número: ");
+        n1 = entrada.nextDouble();
+
+        System.out.println("Digite o segundo número: ");
+        n2 = entrada.nextDouble();
+
+        media = (n1 + n2) / 2.0;
+
+        System.out.printf("Média = %.2f", media );
 
     }
 }
